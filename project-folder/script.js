95343c70a60a7951ef0b4931111b3ac42519a234
mitchell-s1 <166095001+mitchell-s1@users.noreply.github.com>
2025-12-27 15:00:15 -0800

    Delete project-folder directory

--- project-folder/script.js
+++
@@ -1,4 +0,0 @@
-// mobile nav toggle
-const toggle = document.getElementById('mobile-toggle');
-const nav = document.querySelector('nav ul');
-toggle.addEventListener('click', () => nav.classList.toggle('show'));
